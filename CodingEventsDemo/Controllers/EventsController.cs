4f5e0bcfcec1d6d70a1de83de9db971720a044fc
zkrem56 <zkremer702@gmail.com>
2022-10-31 11:39:31 -0500

    first Commit

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,12 +13,13 @@ namespace coding_events_practice.Controllers
     public class EventsController : Controller
     {
 
-        static private List<string> Events = new List<string>();
+        //static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        //static private List<Event> Events = new List<Event>();
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -28,9 +31,39 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         [Route("Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string info)
         {
-            Events.Add(name);
+            /*if (!Events.Contains(new Event(name)))
+            {
+                Events.Add(new Event(name));
+            }
+            else
+            {
+                ViewBag.error = "ERROR";
+                return Redirect("/Events/Add");
+            }*/
+            EventData.Add(new Event(name, info));
+            
+
+            return Redirect("/Events");
+        }
+
+        // GET: /Events/Delete
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        // POST: /Events/Delete
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
 
             return Redirect("/Events");
         }
---
+++ CodingEventsDemo/Data/EventData.cs
@@ -0,0 +1,36 @@
+﻿using CodingEventsDemo.Models;
+using System.Collections.Generic;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventData
+    {
+        // store events
+        private static Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+        // add events
+        public static void Add(Event newEvent) 
+        {
+            Events.Add(newEvent.Id, newEvent);
+        }
+
+        // retrieve events
+        public static IEnumerable<Event> GetAll()
+        {
+            return Events.Values;
+        }
+
+        // retrieve a single event
+        public static Event GetById(int id)
+        {
+            return Events[id];
+        }
+
+        // remove an event
+        public static void Remove(int id)
+        {
+            Events.Remove(id);
+        }
+
+    }
+}
